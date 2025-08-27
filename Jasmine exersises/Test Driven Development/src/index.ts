7dee9167d1ac16c9bec0526e385f3c691e73e537
Muath-Ademar <muazademar20@gmail.com>
2025-08-27 05:02:36 +0300

    test driven development

---
+++ Jasmine exersises/Test Driven Development/src/index.ts
@@ -0,0 +1,60 @@
+/** Use Axios to get data from restcountries api */
+import axios from 'axios';
+
+/** Use the free API https://restcountries.com/
+ * You will use the following endpoints
+ * https://restcountries.com/v2/name/${name} for countries by name
+ * https://restcountries.com/v2/regionalbloc/${regionalbloc} for region blocks
+ */
+
+/** Create getCountry Function here */
+async function getCountry(name: string){
+  const getApi = await axios(`https://restcountries.com/v2/name/${name}`)
+  const data = getApi.data
+  return {
+    capital: data[0].capital,
+    region: data[0].region,
+    numericCode: data[0].numericCode
+  }
+
+  
+}
+
+
+
+
+/** Create a test for this getRegion function */
+async function getRegionCountries(regionalbloc: string) {
+  const getApi = await axios(
+    `https://restcountries.com/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data;
+  const countries = [];
+  for (let i = 0; i < data.length; i++) {
+    countries.push(data[i].name);
+  }
+  return countries;
+}
+
+/** Create getRegionCapitals function here */
+
+async function getRegionCapitals(capital:string) {
+  const getApi = await axios(`https://restcountries.com/v2/regionalbloc/${capital}`)
+  const data = getApi.data
+  const capitals = []
+  for (let i = 0; i<data.length; i++){
+    capitals.push(data[i].capital)
+  }
+  return capitals
+  
+}
+
+getRegionCapitals('nafta')
+
+
+
+export default {
+  getCountry,
+  getRegionCountries,
+  getRegionCapitals
+};
